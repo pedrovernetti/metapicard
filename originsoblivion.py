9f80c23a04b87b74b188f62cd3c6655c981e2fb0
pedrovernetti <pedrovernetti@gmail.com>
2021-05-21 07:35:59 -0300

    Encoders Oblivion renamed and made more aggressive and comprehensive

--- originsoblivion.py
+++ originsoblivion.py
@@ -10,8 +10,8 @@
 # access <http://www.gnu.org/licenses/> to find and read it.
 #
 # Author: Pedro Vernetti G.
-# Name: Encoders Oblivion
-# Description: MusicBrainz Picard plugin to purge encoding-related tags.
+# Name: Origins Oblivion
+# Description: MusicBrainz Picard plugin to purge encoding and software-related tags.
 #
 # #  In order to have this plugin working (if it is currently not), place it at:
 #    ~/.config/MusicBrainz/Picard/plugins
@@ -21,16 +21,16 @@ import re
 
 
 
-PLUGIN_NAME = 'Encoders Oblivion'
+PLUGIN_NAME = 'Origins Oblivion'
 PLUGIN_AUTHOR = 'Pedro Vernetti G.'
-PLUGIN_DESCRIPTION = 'Purges most/all encoding-related tags.'
+PLUGIN_DESCRIPTION = 'Purges most/all encoding and software-related tags.'
 PLUGIN_VERSION = '0.1'
 PLUGIN_API_VERSIONS = ['2.0', '2.1', '2.2', '2.3', '2.4', '2.5', '2.6']
 PLUGIN_LICENSE = 'GPLv3'
 PLUGIN_LICENSE_URL = 'https://www.gnu.org/licenses/gpl-3.0.en.html'
 
 from picard import log
-from picard.file import File
+from picard.file import File, register_file_post_addition_to_track_processor, register_file_post_load_processor
 from picard.metadata import register_track_metadata_processor
 from picard.track import Track
 from picard.ui.itemviews import BaseAction, register_file_action, register_track_action
@@ -52,17 +52,30 @@ class SuperComment( BaseAction ):
         metadata.pop(r'ENCODINGTIME', None)
         metadata.pop(r'major_brand', None)
         metadata.pop(r'minor_version', None)
+        metadata.pop(r'software', None)
+        metadata.pop(r'source', None)
         metadata.pop(r'itunsmpb', None)
+        metadata.pop(r'tbpm', None)
+        metadata.pop(r'tlen', None)
+        metadata.pop(r'tsrc', None)
+
+    def processFile( self, track, file ):
+        self.process(None, file.metadata, track, None)
+
+    def processFileOnLoad( self, file ):
+        self.process(None, file.metadata, None, None)
 
     def callback( self, objs ):
         for obj in objs:
-            if isinstance(obj, Track):
-                for f in obj.linked_files: self.process(None, f.metadata, None, None)
-            elif isinstance(obj, File):
+            if (isinstance(obj, Track)):
+                for f in obj.linked_files: self.process(None, f.metadata, obj, None)
+            elif (isinstance(obj, File)):
                 self.process(None, obj.metadata, None, None)
 
 
 
 register_file_action(SuperComment())
+register_file_post_addition_to_track_processor(SuperComment().processFile)
+register_file_post_load_processor(SuperComment().processFileOnLoad)
 # register_track_action(SuperComment())
-register_track_metadata_processor(SuperComment().process)
+# register_track_metadata_processor(SuperComment().process)
